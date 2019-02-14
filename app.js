2a79e7bffcdab4151461f043e29e03cc2b2b028c
unknown <nbatre1@yahoo.com>
2019-02-14 17:14:57 -0600

    keep moving forward

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ mongoose.connect(db,{useNewUrlParser:true})
 app.use(expressLayouts);
 app.set('view engine','ejs');
 
+//BodyParser
+app.use(express.urlencoded({extended:false}));
 
 // Routes being used
 app.use('/',require('./routes/index'));
